87a9ba84ca39048f5d144abaf392d689e2ca1545
Rezeon <Rheynoair@gmail.com>
2025-08-13 16:35:07 +0700

    Add serverless-http dependency

--- scr/index.js
+++ scr/index.js
@@ -1,7 +1,6 @@
 const serverless = require('serverless-http');
 const app = require('./app');
 require('dotenv').config();
-app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello from Express on Vercel!');
