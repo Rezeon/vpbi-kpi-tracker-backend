d4ee8d7533b55d91b1e3ee498fc45d8f252d7ae3
Rezeon <Rheynoair@gmail.com>
2025-08-13 17:20:58 +0700

    Add serverless-http dependency

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-const express = require('express');
+const app = require('./scr/app');
 const serverless = require('serverless-http');
-
-const app = express();
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello from Express on Vercel!');
-});
-
-app.get('/test', (req, res) => {
-  res.json({ message: 'Test route works!' });
-});
 
 module.exports = app;
 module.exports.handler = serverless(app);
