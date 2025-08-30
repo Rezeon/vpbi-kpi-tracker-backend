497c64be729bd413b911d772c4fe977b0039efc4
Rezeon <Rheynoair@gmail.com>
2025-08-30 12:53:41 +0700

    perbaikan api

--- scr/controllers/penilaianKpi.js
+++ scr/controllers/penilaianKpi.js
@@ -39,6 +39,7 @@ const addPenilaianKpi = asyncHandler(async (req, res) => {
 const getAllPenilaianKpi = asyncHandler(async (req, res) => {
   const penilaianKpi = await prisma.penilaianKPI.findMany({
     include: {
+      karyawanId: true,
       karyawan: true,
       dibuatOleh: true,
       detail: true,
@@ -53,6 +54,7 @@ const getPenilaianKpiById = asyncHandler(async (req, res) => {
   const penilaianKpi = await prisma.penilaianKPI.findUnique({
     where: { id: Number(id) },
     include: {
+      karyawanId: true,
       karyawan: true,
       dibuatOleh: true,
       detail: true,
